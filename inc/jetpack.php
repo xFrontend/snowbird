012d6fd8ab6cd8791fa5fc9e0f24a891a2227146
Omaar Osmaan <omo@moonomo.com>
2016-09-08 01:19:10 +0600

    Honors layout settings to load Posts w/ Jetpack Infinite Scroll. Fixes #19.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -31,6 +31,14 @@ add_action( 'after_setup_theme', 'snowbird_jetpack_setup' );
 function snowbird_jetpack_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
-		get_template_part( 'template-parts/content', get_post_format() );
+
+		/**
+		 * Load Posts Content based on Setting.
+		 */
+		if ( 'alternate' == Snowbird()->mod( 'loop_layout_type' ) ) :
+			get_template_part( 'template-parts/content-alternate', get_post_format() );
+		else:
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
 	}
 }
