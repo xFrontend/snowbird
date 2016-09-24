627b9fb6d5a39b504b3d193e5e2429c1bd00ce70
tfrommen <info@tfrommen.de>
2016-09-24 22:58:14 +0200

    Simplify printf expression.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -15,7 +15,7 @@
 
 			<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-				<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'snowbird' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+				<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'snowbird' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 			<?php elseif ( is_search() ) : ?>
 
