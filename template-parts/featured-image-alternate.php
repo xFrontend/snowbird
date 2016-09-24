96af8567beeaa53bdd55070a5e260a11a5abba8b
tfrommen <info@tfrommen.de>
2016-09-24 23:13:08 +0200

    Formatting.

--- template-parts/featured-image-alternate.php
+++ template-parts/featured-image-alternate.php
@@ -5,12 +5,11 @@
 if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() ) : ?>
 <div class="xf__container">
 	<figure class="xf__feaured-image xf__feaured-image-alt" aria-hidden="true">
-		<?php
-		if ( is_singular() ) {
+		<?php if ( is_singular() ) {
 
 			the_post_thumbnail( 'snowbird-large', array(
 				'alt'      => get_the_title(),
-				'itemprop' => 'image'
+				'itemprop' => 'image',
 			) );
 
 		} else {
@@ -20,7 +19,7 @@ if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() ) :
 				esc_url( get_the_permalink() ),
 				get_the_post_thumbnail( get_the_ID(), 'snowbird-large', array(
 					'alt'      => get_the_title(),
-					'itemprop' => 'image'
+					'itemprop' => 'image',
 				) )
 			);
 
