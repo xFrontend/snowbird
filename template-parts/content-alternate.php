3bb3f37083f1a4f34c7d749822962bc34e109acd
tfrommen <info@tfrommen.de>
2016-10-10 22:39:14 +0200

    Revert formatting.

--- template-parts/content-alternate.php
+++ template-parts/content-alternate.php
@@ -6,7 +6,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemtype="http://schema.org/BlogPosting"
-	itemscope="itemscope" itemprop="blogPost">
+         itemscope="itemscope" itemprop="blogPost">
 
 	<div class="xf__container">
 		<header class="xf__post-header">
